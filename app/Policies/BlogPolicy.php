6d24f9dd792b660b81998881abd4f61ccf749bf0
lvt <zero.lethanh@gmail.com>
2016-10-17 02:18:34 +0900

    add blog Policy

---
+++ app/Policies/BlogPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Blog;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BlogPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function edit(User $user, Blog $blog)
+    {
+        return (int)$user->id === (int)$blog->user_id;
+    }
+}
