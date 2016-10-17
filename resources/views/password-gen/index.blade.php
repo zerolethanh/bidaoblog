32a6ac969a9213a313f7c1398f60e4b1f1a7a50d
lvt <zero.lethanh@gmail.com>
2016-10-18 02:15:37 +0900

    update password gen

--- resources/views/password-gen/index.blade.php
+++ resources/views/password-gen/index.blade.php
@@ -6,57 +6,60 @@
     <div class="container">
         <div class="row">
 
-                <div class="form-group">
-                    <label for="pw" class="col-sm-2 control-label">Password: </label>
-                    <div class="col-sm-10">
-                        <div class="input-group">
-                            <input id="pw" class="form-control" value="{{$random_pw}}"/>
-                            <div class="input-group-btn">
-                                <button data-copytarget="pw" class="btn btn-success" onclick="copy()">Copy</button>
-                            </div>
+            <div class="form-group">
+                <label for="pw" class="col-sm-2 control-label">Password: </label>
+                <div class="col-sm-10">
+                    <div class="input-group">
+                        <input id="pw" class="form-control" value="{{$random_pw}}"/>
+                        <div class="input-group-btn">
+                            <button data-copytarget="pw" class="btn btn-success" onclick="copy()">Copy</button>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="len" class="col-sm-2 control-label">Password Length: </label>
-                    <div class="col-sm-2">
-                        <input type="number" class="col-sm-10 form-control " name="len" id="len" value="20">
+            <div class="form-group">
+                <label for="len" class="col-sm-2 control-label">Password Length: </label>
+                <div class="col-sm-2">
+                    <input type="number" class="col-sm-10 form-control " name="len" id="len" value="20">
+                </div>
+            </div>
+            <br><br><br>
+            <div class="form-group">
+                <div class="col-sm-offset-2">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="l" id="l" checked> abcdefghjkmnpqrstuvwxyz
+                        </label>
+                    </div>
+
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="u" id="u" checked> ABCDEFGHJKMNPQRSTUVWXYZ
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="d" id="d" checked> 0123456789
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="s" id="s" checked> !@#$%&*?
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="add_dashes" id="add_dashes"> add dashes
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <button class="btn btn-danger" onclick="query()">Regenerate</button>
+                        </label>
                     </div>
                 </div>
-                <br>
-                <br>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="l" id="l" checked> abcdefghjkmnpqrstuvwxyz
-                    </label>
-                </div>
-
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="u" id="u" checked> ABCDEFGHJKMNPQRSTUVWXYZ
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="d" id="d" checked> 0123456789
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="s" id="s" checked> !@#$%&*?
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="add_dashes" id="add_dashes"> add dashes
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <button class="btn btn-danger" onclick="query()">Regenerate</button>
-                    </label>
-                </div>
+            </div>
         </div>
 
     </div>
